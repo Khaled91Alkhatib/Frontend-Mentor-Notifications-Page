13486eea4ec4be4f48719c54efcc9bb6cf54024d
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2023-02-26 16:16:45 -0500

    Add more data and apply states to handle conditionals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 import markImage from './Assets/images/avatar-mark-webber.webp';
+import angelaImage from './Assets/images/avatar-angela-gray.webp';
+import jacobImage from './Assets/images/avatar-jacob-thompson.webp';
+import rizkyImage from './Assets/images/avatar-rizky-hasanuddin.webp';
+import kimberlyImage from './Assets/images/avatar-kimberly-smith.webp';
+import nathanImage from './Assets/images/avatar-nathan-peterson.webp';
+import annaImage from './Assets/images/avatar-anna-kim.webp';
+
+import chessImage from './Assets/images/image-chess.webp';
 
 function App() {
+  const [allRead, setAllRead] = useState(false);
+
   return (
     <div className='main-container'>
       <div>
@@ -11,18 +21,119 @@ function App() {
           <div className='all-nav'>
             <div className='notification-bar'>
               <div style={{ fontWeight: "800", marginRight: '10px', fontSize: '20px' }}>Notifications</div>
-              <div className='notification-number'>3</div>
-            </div>
-            <div className='mark-as-read'>Mark all as read</div>
-          </div>
-          <div className='single-feed'>
-            <div className='image-info'>
-              <img src={markImage} alt="mark" />
-              <div>
-                <div className='info'><strong style={{ color: "black", marginRight: "2px" }}>Mark Webber</strong> reacted to your recent post <strong>My first tournament today!</strong></div>
-                <div className='post-time'>1m ago</div>
+              <div className='notification-number'>
+                {allRead === true ? 0 : 3}
               </div>
             </div>
+            <button onClick={() => setAllRead(true)} className='mark-as-read'>Mark all as read</button>
+          </div>
+          <div className='single-feed'>
+            {allRead === true ?
+              <>
+                <div className='image-info'>
+                  <img src={markImage} alt="mark" />
+                  <div>
+                    <div className='info'><strong className='name'>Mark Webber</strong> reacted to your recent post <strong className='group'>&nbsp; My first tournament today!</strong></div>
+                    <div className='post-time'>1m ago</div>
+                  </div>
+                </div>
+
+                <div className='image-info'>
+                  <img src={angelaImage} alt="mark" />
+                  <div>
+                    <div className='info'><strong className='name'>Angela Gray</strong> followed you</div>
+                    <div className='post-time'>5m ago</div>
+                  </div>
+                </div>
+
+                <div className='image-info'>
+                  <img src={jacobImage} alt="mark" />
+                  <div>
+                    <div className='info'><strong className='name'>Jacob Thompson</strong> has joined your group <strong className='group'>&nbsp; Chess Club</strong></div>
+                    <div className='post-time'>1 day ago</div>
+                  </div>
+                </div>
+              </>
+              :
+              <>
+                <div className='image-info unread-post'>
+                  <img src={markImage} alt="mark" />
+                  <div>
+                    <div style={{ display: 'flex', justifyContent: "center", alignItems: 'center' }}>
+                      <div className='info'><strong className='name'>Mark Webber</strong> reacted to your recent post <strong className='group'>&nbsp; My first tournament today!</strong></div>
+                      <div className='unread-dot' />
+                    </div>
+                    <div className='post-time'>1m ago</div>
+                  </div>
+                </div>
+
+                <div className='image-info unread-post' >
+                  <img src={angelaImage} alt="mark" />
+                  <div>
+                    <div style={{ display: 'flex', justifyContent: "center", alignItems: 'center' }}>
+                      <div className='info'><strong className='name'>Angela Gray</strong> followed you</div>
+                      <div className='unread-dot' />
+                    </div>
+                    <div className='post-time'>5m ago</div>
+                  </div>
+                </div>
+
+                <div className='image-info unread-post'>
+                  <img src={jacobImage} alt="mark" />
+                  <div>
+                    <div style={{ display: 'flex', justifyContent: "center", alignItems: 'center' }}>
+                      <div className='info'><strong className='name'>Jacob Thompson</strong> has joined your group <strong className='group'>&nbsp; Chess Club</strong></div>
+                      <div className='unread-dot' />
+                    </div>
+                    <div className='post-time'>1 day ago</div>
+                  </div>
+                </div>
+              </>
+            }
+
+            <div className='image-info'>
+              <img src={rizkyImage} alt="mark" />
+              <div>
+                <div className='info'><strong className='name'>Rizky Hasanuddin</strong> sent you a private message</div>
+                <div className='post-time'>5 days ago</div>
+                <div className='message'>
+                  Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and
+                  I'm already having lots of fun and improving my game.
+                </div>
+              </div>
+            </div>
+
+            <div className='image-info'>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div style={{ display: 'flex' }}>
+                  <img src={kimberlyImage} alt="mark" />
+                  <div>
+                    <div className='info'><strong className='name'>Kimberly Smith</strong> commented on your picture</div>
+                    <div className='post-time'>1 week ago</div>
+                  </div>
+                </div>
+                <div>
+                  <img style={{ margin: '0' }} src={chessImage} alt="chess" />
+                </div>
+              </div>
+            </div>
+
+            <div className='image-info'>
+              <img src={nathanImage} alt="mark" />
+              <div>
+                <div className='info'><strong className='name'>Nathan Peterson</strong> reacted to your recent post <strong className='group'>&nbsp; 5 end-game strategies to increase your win rate</strong></div>
+                <div className='post-time'>2 weeks ago</div>
+              </div>
+            </div>
+
+            <div style={{ paddingBottom: '0', marginBottom: '0' }} className='image-info'>
+              <img src={annaImage} alt="mark" />
+              <div>
+                <div className='info'><strong className='name'>Anna Kim</strong> left the group <strong className='group'>&nbsp; Chess Club</strong></div>
+                <div className='post-time'>5m ago</div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
